fe707745200c81acba4bde8048309f23f7ecc087
yunanmu <yuanmua12@gmail.com>
2023-07-22 15:27:45 +0800

    登录界面做好了一半

--- ssh-ui/src/App.vue
+++ ssh-ui/src/App.vue
@@ -3,17 +3,20 @@ import Terminal from "./views/Terminal.vue";
 import FormView from "@/views/Formview.vue";
 import ElFormAbc from "@/views/ElFormabc.vue";
 import MenuSidebar from "@/views/element/MenuSidebar.vue";
+import LoginVive from "./views/page/login/login.vue";
 </script>
 
 <template><!--
  <ElFormAbc></ElFormAbc>
  <FormView></FormView>-->
- <div class="sidebar">
+<!-- <div class="sidebar">
   <MenuSidebar></MenuSidebar>
  </div>
  <div class="terminal-main">
   <Terminal></Terminal>
- </div>
+ </div>-->
+<!--  <router-view></router-view>-->
+<loginVive></loginVive>
 </template>
 
 <style scoped>
