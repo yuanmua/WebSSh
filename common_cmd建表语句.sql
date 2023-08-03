895d5d8c218f8d784a962fed07553dfadebd0659
wan zhang qing <1875427705@qq.com>
2023-08-04 00:15:08 +0800

    新增常用指令集增删改查的相关代码和建表语句，但未进行相应测试

---
+++ common_cmd建表语句.sql
@@ -0,0 +1,14 @@
+use ssh;
+CREATE TABLE `common_cmd` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '组名',
+   `user_id` bigint NOT NULL COMMENT '用户id',
+   `server_id` bigint NOT NULL COMMENT '服务器id',
+
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `cmds` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '指令集',
+
+  PRIMARY KEY (`id`)
+
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin COMMENT='常用命令';
