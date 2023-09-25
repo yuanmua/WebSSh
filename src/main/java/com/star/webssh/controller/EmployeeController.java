c1fed8e77123397d7d02e2de103d0ad21373b752
Wr.Wang <3169927892@qq.com>
2023-09-25 18:02:30 +0800

    新增自动生成文档knife4j 增强了加密功能 前端访问接口： http://localhost:8000/doc.html

--- src/main/java/com/star/webssh/controller/EmployeeController.java
+++ src/main/java/com/star/webssh/controller/EmployeeController.java
@@ -2,19 +2,21 @@ package com.star.webssh.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.star.webssh.common.BaseContext;
+import com.star.webssh.common.JWTUtils;
 import com.star.webssh.common.R;
 import com.star.webssh.pojo.Employee;
 import com.star.webssh.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Mr.Wang
@@ -24,6 +26,11 @@ import java.time.LocalDateTime;
 @Slf4j
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
 
     @Autowired
     private EmployeeService empService;
@@ -50,5 +57,57 @@ public class EmployeeController {
         empService.save(employee);
 
         return R.success("注册成功");
+    }
+
+    /**
+     * 退出登录
+     * @return
+     */
+
+    @PostMapping("/logout")
+    public R<String> logout() {
+
+        // 清除cookie，并且退出
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+        return R.success("推出成功");
+
+
+    }
+
+
+    /**
+     * 登录
+     * @param e
+     * @return
+     */
+    @PostMapping("/login")
+    public R login(@RequestBody Employee e) {
+
+        //将密码加密后进行检验
+        e.setPassword(DigestUtils.md5DigestAsHex(e.getPassword().getBytes()));
+        LambdaQueryWrapper<Employee> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(e.getUsername()!=null,Employee::getUsername,e.getUsername());
+
+
+        //查询到当前登录用户在表中的信息
+        lqw.eq(e.getPassword()!=null,Employee::getPassword,e.getPassword());
+        Employee emp=empService.getOne(lqw);
+        if(emp!=null){
+            Map<String,Object > claim=new HashMap<>();
+            claim.put("username",emp.getUsername());
+            claim.put("id",emp.getId());
+            claim.put("password",emp.getPassword());
+            String jwt = JWTUtils.createJWT(claim);
+
+            return R.success(jwt);
+        }
+        else {
+            return R.error("密码错误或用户名错误");
+        }
+
     }
 }
